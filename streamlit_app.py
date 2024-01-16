c36f6fdc6c13c1e88b970367c6aef27ceb58c62c
depagadala <156818352+depagadala@users.noreply.github.com>
2024-01-16 10:04:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
-streamlit.title("My first streamlist application")
+streamlit.title("My Parents New Healthy Diner")
+streamlit.header("Breakfast Menu!!")
+streamlit.text("Omega 3 & Bluberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-boiled free range eggs")
